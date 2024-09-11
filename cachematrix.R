6fd7e145c03fea682c466e49b33c119755442ee5
Jack Wang <drwangzhen@gmail.com>
2024-09-11 16:01:40 -0400

    Write a short comment

--- cachematrix.R
+++ cachematrix.R
@@ -3,12 +3,18 @@
 
 ## Write a short comment describing this function
 
+## This function creates a special matrix that stores its inverse. 
+## You can set and get the matrix and its inverse, which speeds up calculations when you need the inverse multiple times.
+
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
 ## Write a short comment describing this function
+
+## The cacheSolve function finds the inverse of a matrix created by makeCacheMatrix. 
+## It checks if the inverse is already cached; if so, it uses the cached value. If not, it calculates the inverse, stores it, and then returns it.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
